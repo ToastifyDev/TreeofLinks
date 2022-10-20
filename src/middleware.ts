cfbb48dee044cf46754e3a6cb6d7dadd2124c044
Not TCA <realtcatech@gmail.com>
2022-10-20 11:26:31 +0800

    readd slugs

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const slug = req.nextUrl.pathname.split("/").pop();
+
+  const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
+  if (slugFetch.status === 404) return NextResponse.next();
+  const data = await slugFetch.json();
+
+  if (data?.id)
+    return NextResponse.rewrite(new URL(`/user/${data?.id}`, req.url));
+}
+
+export const config = {
+  matcher: "/:slug*",
+};
